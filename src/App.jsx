c5fa17b4359bcaf4609133dca7cd09662f87c48b
mrybaruk <mykola.rybaruk@pergamonmed.com>
2023-10-27 15:31:49 +0200

    Added state for conditionally render project generatior or project list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [propjectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartNewProjest() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (propjectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (propjectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartNewProjest} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartNewProjest} />
+      {content}
     </main>
   );
 }

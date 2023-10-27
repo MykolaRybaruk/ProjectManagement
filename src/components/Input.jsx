902639116c929fe0fb952f4bf3af463eaabdaa78
mrybaruk <mykola.rybaruk@pergamonmed.com>
2023-10-27 11:40:34 +0200

    Styled components

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,7 @@
 export default function Input({ label, textarea, ...props }) {
-    return <p>
-        <labell>{label}</labell>
-        {textarea ? <textarea {...props} /> : <input {...props} />}
+    const styles ='w-full p-1 border-b-2 rounded borer-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600';
+    return <p className="flex flex-col gap-1 my-4">
+        <label className="text-sm uppercase text-stone-500">{label}</label>
+        {textarea ? <textarea className={styles} {...props} /> : <input className={styles} {...props} />}
     </p>
 }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,11 +1,11 @@
 import Input from "./Input";
 
 export default function NewProject() {
-    return <div>
-        <menu>
+    return <div className="w-[45rem] mt-16">
+        <menu className="flex items-center justify-center gap-4 my-4">
             <li>
-                <button>Cancel</button>
-                <button>Save</button>
+                <button className="text-stone-800 hover:text-stone-850">Cancel</button>
+                <button className="px-6 py-2 rounded-md bg-stone:800 text-stone:50 hover:bg-stone-950">Save</button>
             </li>
         </menu>
         <div>

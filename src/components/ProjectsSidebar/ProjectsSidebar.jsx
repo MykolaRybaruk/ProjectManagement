629f7394ec92eec2234ea299d52f073155784104
mrybaruk <mykola.rybaruk@pergamonmed.com>
2023-10-27 10:48:51 +0200

    added  project sidebar component

---
+++ src/components/ProjectsSidebar/ProjectsSidebar.jsx
@@ -0,0 +1,9 @@
+export default function ProjectsSidebar(props) {
+    return <aside>
+        <h2>Your Projects!</h2>
+        <button>
+            + Add Project
+        </button>
+        <ul>...</ul>
+    </aside>
+}
